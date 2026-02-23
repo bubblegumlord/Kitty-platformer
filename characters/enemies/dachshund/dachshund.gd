46ccfcbc090b244075e6e75dd30cad3d46e71065
bubblegumlord <102076526+bubblegumlord@users.noreply.github.com>
2026-02-23 22:07:19 +0100

    dachshund enemy

---
+++ characters/enemies/dachshund/dachshund.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+const MOVE_RANGE: int = 200
+
+@export var direction: int
+
+var retract: bool = false
+var rad_dir: float
+var move_vector: Vector2
+var move: Tween
+var new_position: Vector2
+
+func _ready() -> void:
+	if direction % 90 != 0:
+		print("Jamnik " + str(global_position) + " niewłaściwy kierunek")
+	
+	rad_dir = deg_to_rad(direction)
+	move_vector = Vector2(cos(rad_dir), sin(rad_dir))
+	new_position = move_vector * MOVE_RANGE
+
+func _on_timer_timeout() -> void:
+	if not retract:
+		retract = true
+		move = get_tree().create_tween()
+		move.tween_property(self, "global_position", global_position + new_position, 0.5)
+	else:
+		retract = false
+		move = get_tree().create_tween()
+		move.tween_property(self, "global_position", global_position - new_position, 0.5)
--- globals/globals.gd
+++ globals/globals.gd
@@ -5,6 +5,7 @@ var saved_items: Dictionary[int, bool]
 
 var active_checkpoint: Vector2
 var checkpoint_level: Node2D
+var level: String
 
 func _ready() -> void:
 	for i: int in range(1,100):
@@ -13,5 +14,5 @@ func _ready() -> void:
 	saved_items = items
 
 func change_level() -> void:
-	var level: String = checkpoint_level.scene_file_path
+	level = checkpoint_level.scene_file_path
 	get_tree().change_scene_to_file(level)
